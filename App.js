3ea7f98283b2e005ea99b3c4357a7a2b434b7ddc
sw <genie_s2@naver.com>
2018-12-20 15:59:52 +0900

    AsyncStorage

--- App.js
+++ App.js
@@ -7,7 +7,8 @@ import {
     TextInput,
     Dimensions,
     Platform,
-    ScrollView
+    ScrollView,
+    AsyncStorage
 } from "react-native";
 import { AppLoading } from "expo";
 import ToDo from "./ToDo";
@@ -90,6 +91,7 @@ export default class App extends React.Component {
                         ...newToDoObject
                     }
                 };
+                this._saveToDos(newState.toDos);
                 return { ...newState };
             });
         }
@@ -104,6 +106,7 @@ export default class App extends React.Component {
             };
             return { ...newState };
         });
+        this._saveToDos(newState.toDos);
     };
     _uncompleteToDo = id => {
         this.setState(prevState => {
@@ -117,6 +120,7 @@ export default class App extends React.Component {
                     }
                 }
             };
+            this._saveToDos(newState.toDos);
             return { ...newState };
         });
     };
@@ -132,6 +136,7 @@ export default class App extends React.Component {
                     }
                 }
             };
+            this._saveToDos(newState.toDos);
             return { ...newState };
         });
     };
@@ -147,8 +152,15 @@ export default class App extends React.Component {
                     }
                 }
             };
+            this._saveToDos(newState.toDos);
             return { ...newState };
         });
+    };
+    _saveToDos = newToDos => {
+        const saveToDos = AsyncStorage.setItem(
+            "toDos",
+            JSON.stringify(newToDos)
+        );
     };
 }
 
